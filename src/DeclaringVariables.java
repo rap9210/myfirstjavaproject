fc40ca80dbe924519fcf2bea9d4bac2373d6b9c6
rap9210 <rap9210@gmail.com>
2020-04-14 10:47:45 -0400

    April 14 Project

---
+++ src/DeclaringVariables.java
@@ -0,0 +1,13 @@
+public class DeclaringVariables {
+    public static void main(String[] argv){
+        int Test_Score = 94;
+        double Banana_Price = 0.50;
+        String Test_Taker = "Ron Swanson";
+
+        System.out.println(Test_Taker);
+        System.out.println("SCORE: "+ Test_Score);
+        System.out.println("In other unrelated news, " +
+                "the price of a banana is $"+ Banana_Price);
+
+    }
+}
---
+++ src/RJava.java
@@ -0,0 +1,24 @@
+public class RJava {
+
+    public static void main(String[] args) {
+        boolean torf = true;
+        byte B = 4;
+        char a = 'A';
+        short index = 3;
+        int i = 76;
+        float flt = 7/3;
+        long lng = 7698000;
+        double dbl = 0.33333;
+
+        System.out.println("torf: "+ torf);
+        System.out.println("B: "+ B);
+        System.out.println("a: "+ a);
+        System.out.println("index: "+ index);
+        System.out.println("i: "+ i);
+        System.out.println("flt: "+ flt);
+        System.out.println("lng: "+ lng);
+        System.out.println("double: "+ dbl);
+
+
+    }
+}
